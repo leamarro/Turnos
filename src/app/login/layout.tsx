8f82ba048511ef50c7b5a6edbe871b65e3c5632f
Leandro Marrocchi <79809924+Leamarro@users.noreply.github.com>
2025-12-23 13:12:40 -0300

    Refactor LoginLayout to remove outer div

--- src/app/login/layout.tsx
+++ src/app/login/layout.tsx
@@ -1,9 +1,5 @@
 import { ReactNode } from "react";
 
 export default function LoginLayout({ children }: { children: ReactNode }) {
-  return (
-    <div className="min-h-screen flex items-center justify-center">
-      {children}
-    </div>
-  );
+  return <>{children}</>;
 }
