28e55eb2fc39c23f9f5178863873dd30c3526d95
Leandro Marrocchi <79809924+leamarro@users.noreply.github.com>
2026-08-07 09:25:58 -0300

    Add custom headers for service worker

--- next.config.js
+++ next.config.js
@@ -2,6 +2,16 @@
 const nextConfig = {
   // sin reactCompiler
   reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: "/sw.js",
+        headers: [
+          { key: "Cache-Control", value: "no-cache, no-store, must-revalidate" },
+        ],
+      },
+    ];
+  },
 }
 
 module.exports = nextConfig
